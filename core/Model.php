931a1d0de5f1a8edbc6f1f4342c4bbaa9fa7b057
iya30n <varmeziyaryasin@gmail.com>
2019-06-03 12:23:37 +0430

    add model parent class and models directory

---
+++ core/Model.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Core;
+
+use App\Core\App;
+
+class Model
+{
+    protected static $table;
+
+    public static function all()
+    {
+        return App::get('database')->selectAll(static::$table);
+    }
+
+    public static function create($values)
+    {
+        App::get('database')->insert(static::$table, $values);
+    }
+
+    public static function find($id)
+    {
+        return App::get('database')->find(static::$table, $id);
+    }
+
+    public static function update($id, $values)
+    {
+        App::get('database')->update(static::$table, $id, $values);
+    }
+
+    public static function delete($id)
+    {
+        App::get('database')->delete(static::$table, $id);
+    }
+}
+
